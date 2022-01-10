1bf123ef9ce59322d086d5cff04ff932c8d51dad
Seif Enan <seifenan@gmail.com>
2022-01-10 12:08:20 -0500

    modified models-inc; switching to dt

--- models/index.js
+++ models/index.js
@@ -6,11 +6,31 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
 // Categories have many Products
+
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
 
 module.exports = {
   Product,
